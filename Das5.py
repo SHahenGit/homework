21a03564bf80531af49da4e61c0a8cb87352b4fd
SHahenGit <mehrabyanshahen@gmail.com>
2019-09-23 20:22:52 +0400

    Client age indicator change 1

---
+++ Das5.py
@@ -0,0 +1,43 @@
+'''mnacord = -6%5
+print(mnacord)
+
+
+
+a = 7
+b = 5
+amboghj_mas = a // b
+print(amboghj_mas)
+
+
+x = 5
+x += 4
+print(x)
+
+x = 5
+x //= 4
+print(x)
+
+x = 5
+x **= 4
+print(x)
+
+x = 25
+x =int(x**(0.5))
+print(x)'''
+
+
+'''b = 5
+c = 7
+d = -6
+
+print((a*b) >= (c/d))
+print((a*b) >= (c/d) and (a//b) == (c/d))
+print((a*b) >= (c/d) or (a//b) == (c/d))
+print((a%b) >= (c**d) and (a//b) == (c+d))
+print((a//b) >= (c**d) or (a//b) == (c+d))'''
+
+
+client_name = input("Please tell me your name")
+##print("Ok" + client_name + "Now type Your Year of birth and I will tell You when you will pass your 100 ")
+client_birth_year = int(input("Ok" + client_name + "Now type Your Year of birth and I will tell You when you will pass your 100 "))
+print(client_birth_year + 100 - 2019)
