daf83204aa272505bec6db5ab1b4d2caa1856fc5
SHahenGit <mehrabyanshahen@gmail.com>
2019-09-25 19:46:10 +0400

    1

---
+++ Das6.py
@@ -0,0 +1,15 @@
+a = int(input("number 1: "))
+b = int(input("number 2: "))
+c = int(input("number 3: "))
+
+if a < b:
+	print("A is less than B")
+
+if a > b:
+	print("A is greater than B")
+
+if a == b and c > 7:
+	print("A is equal B")
+
+else:
+	print("gggg")
