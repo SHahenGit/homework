a8ca793e78d21c65517c7ffa0c3739fe0f02a5f8
SHahenGit <mehrabyanshahen@gmail.com>
2019-09-25 14:43:39 +0400

    3 exercises

---
+++ homework5.py
@@ -0,0 +1,19 @@
+
+# exercises 1
+number =int(input("type a number"))
+odd_or_even = (number%2 == 0 and print("even") or number%2 != 0 and print("odd"))
+
+
+#exercises 2
+
+circle_radius = float(input("enter circle radius "))
+circle_area = 3.14 * (circle_radius**2)
+print("your cirlce area is ", circle_area)
+
+
+#exercises 3
+
+import datetime
+
+print("Current date and time : ", datetime.datetime.now())
+
