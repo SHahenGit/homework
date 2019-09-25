16a282b5420cff6063bdd40578d14582fa6afbdd
SHahenGit <mehrabyanshahen@gmail.com>
2019-09-25 20:10:43 +0400

    nested conditions

--- Das6.py
+++ Das6.py
@@ -1,4 +1,4 @@
-a = int(input("number 1: "))
+'''a = int(input("number 1: "))
 b = int(input("number 2: "))
 c = int(input("number 3: "))
 
@@ -12,4 +12,25 @@ if a == b and c > 7:
 	print("A is equal B")
 
 else:
-	print("gggg")
+	print("gggg")'''
+
+
+
+weather = "cold"
+
+a = 5
+b = 3
+
+
+message = "hello world"
+length_of_message = len(message)
+
+print(length_of_message)
+
+if weather == "cold" and a > b and length_of_message > 3:
+	print("it works")
+	if b < 1:
+		print("b is greater than 1 ")
+	print("if statment ends there")
+
+print("our code ends")
