f234cfd47641fc3a376486cb5adf5666aece5660
SHahenGit <mehrabyanshahen@gmail.com>
2019-09-25 21:16:48 +0400

    change 3

--- Das6.py
+++ Das6.py
@@ -33,19 +33,46 @@ if weather == "cold" and a > b and length_of_message > 3:
 		print("b is greater than 1 ")
 	print("if statment ends there")
 
-print("our code ends")'''
+print("our code ends")
 
 
 
 
 a = int(input("number 1: "))
-b = int(input("number 2: "))
 
-if a > b:
-	print("a > b")
 
-elif a < b:
-	print("a < b")
-
+if a >= 0:
+	if a == 0:
+		print("a = 0")
+	else:
+		print("a > 0")
+	
 else:
-	print("a = b")
+	print("a < 0") '''
+
+
+temp = int(input("air temperature: "))
+
+if temp <= 10:
+	cloth = input("what you wear? ")
+
+	if cloth == "hot jaket":
+		print("its ok, have a nice day")
+	elif cloth == "jaket":
+		print("may be you will freeze lil bit ")
+	else:
+		print("be carefuul ")
+elif temp > 10 and temp < 25:
+	print("the weather is warm")
+else:
+	question = input("Do you know Gagik Surenyan ")
+
+	if question == "yes" or question == "may be":
+		print("then ask him what to wear")
+	else:
+		print("have a good day")
+
+
+
+
+
