35ac448f5e7c484f20c52df1578a10be178ce2fe
SHahenGit <mehrabyanshahen@gmail.com>
2019-09-27 20:13:37 +0400

    while loop example

--- Das7.py
+++ Das7.py
@@ -4,7 +4,7 @@
 
 
 
-for i in range(10, 1, -1):
+'''for i in range(10, 1, -1):
 	print("hello", i)
 
 
@@ -13,4 +13,29 @@ print("")
 
 for i in range(0, 10, 2):
 	print("hello", i)
+
+
+sum = 0
+a = 0
+
+for i in range(1, 10, 2):
+	next_number = int(input("enter number " + str(i) + ": "))
+	sum += next_number
+	a += 1
+
+print(sum/a)
+
+
+some_string = "hello world"
+
+for i in some_string:
+	print(i)'''
+
+
+
+i = 7
+
+while  i <= 10:
+	print(i)
+	i += 1
 
