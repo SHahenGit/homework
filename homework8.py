b16ead3386561751ea705e93e8855188d081fdbc
SHahenGit <mehrabyanshahen@gmail.com>
2019-10-01 17:05:35 +0400

    last save

--- homework8.py
+++ homework8.py
@@ -1,43 +1,43 @@
 # exercise 1
 
-# def find_max_number(num1, num2, num3):
+def find_max_number(num1, num2, num3):
 
-# 	if num1 >= num2:
-# 		num = num1		
-# 	else:
-# 		num = num2
+	if num1 >= num2:
+		num = num1		
+	else:
+		num = num2
 
-# 	if num >= num3:
-# 		return num
-# 	else:
-# 		return num3
+	if num >= num3:
+		return num
+	else:
+		return num3
 
-# num1 = int(input("Enter num1: "))
-# num2 = int(input("Enter num2: "))
-# num3 = int(input("Enter num3: "))
+num1 = int(input("Enter num1: "))
+num2 = int(input("Enter num2: "))
+num3 = int(input("Enter num3: "))
 
-# print(find_max_number(num1, num2, num3))
+print(find_max_number(num1, num2, num3))
 
 # exercise 2
 
 
-# def fizz_buzz(num):
-# 	if num % 3 == 0 and num % 5 == 0:
-# 		return "FizzBuzz"
+def fizz_buzz(num):
+	if num % 3 == 0 and num % 5 == 0:
+		return "FizzBuzz"
 
-# 	elif num % 3 == 0:
-# 		return "Fizz"
+	elif num % 3 == 0:
+		return "Fizz"
 
-# 	elif num % 5 == 0:
-# 		return "Buzz"	
+	elif num % 5 == 0:
+		return "Buzz"	
 
-# 	else:
-# 		return num
+	else:
+		return num
 
 
-# num = int(input("Enter a number: "))
+num = int(input("Enter a number: "))
 
-# print(fizz_buzz(num))
+print(fizz_buzz(num))
 
 # exercise 3
 
