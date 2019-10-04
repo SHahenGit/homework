3c382227cfbd3a65633ad6817d27b71dae202f04
SHahenGit <mehrabyanshahen@gmail.com>
2019-10-04 20:25:28 +0400

    try/except

---
+++ Das10.py
@@ -0,0 +1,32 @@
+# try:
+# 	1 / 0
+# except:
+# 	print("you cant divide by zero")
+
+
+# try:
+# 	print ("good")
+# except:
+# 	print("sintax error")
+
+
+my_string = "this string is not a number!"
+
+try:
+	print("converting my string to int...")
+	1/0
+	print("string #" + 1 +  ": " + my_string)
+	my_int = int(my_string)
+	print(my_int)
+except ValueError:
+	print("can't convert; my_string to a number")
+
+except TypeError:
+	print("can't concatinate number with string")
+
+
+except:
+	print("Unknown error")
+
+
+print("Done!")
