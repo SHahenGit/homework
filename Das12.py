234a6ba68b7b64b9641d9bc229e91362081677ab
SHahenGit <mehrabyanshahen@gmail.com>
2019-10-09 19:27:55 +0400

    tuple

---
+++ Das12.py
@@ -0,0 +1,22 @@
+# tuple
+
+# a = (1, 2)
+
+# print(a)
+
+# print(a[0], a[1])
+
+# def tuple1():
+# 	user_name = input("enter your name: ")
+# 	user_surname = input("enter your surname: ")
+# 	return user_name, user_surname
+
+# user_info = tuple1()
+# print("Your name is " + user_info[0])
+# print("Your surname is " + user_info[1])
+
+
+nested_tuple = ((1, 2), (3, 4), (5, 6))
+
+print(nested_tuple[0])
+print(nested_tuple[0][0])
