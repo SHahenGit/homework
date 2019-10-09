c412f6ba6c409e28917ab20167ff43a85fee538a
SHahenGit <mehrabyanshahen@gmail.com>
2019-10-09 19:55:40 +0400

    List

--- Das12.py
+++ Das12.py
@@ -16,7 +16,31 @@
 # print("Your surname is " + user_info[1])
 
 
-nested_tuple = ((1, 2), (3, 4), (5, 6))
+# nested_tuple = ((1, 2), (3, 4), (5, 6))
 
-print(nested_tuple[0])
-print(nested_tuple[0][0])
+# print(nested_tuple[0])
+# print(nested_tuple[0][0])
+
+
+# List
+
+my_list = [1,0,2,3,4,5,6,7,8,9,10,]
+
+my_list.remove(10)
+print(my_list, ": After removing 10")
+
+my_list.sort()
+print(my_list, ": After sorting")
+
+my_list.reverse()
+print(my_list, ": After reversing")
+
+
+a = my_list.pop()
+print(my_list, ": poping")
+print(a)
+
+
+del my_list[-5:]
+print(my_list, ": After deleting the last five items")
+
