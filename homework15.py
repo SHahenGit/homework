cbff476c8454295973889e7bccc0d13fc9bdbb88
SHahenGit <mehrabyanshahen@gmail.com>
2019-10-16 17:51:50 +0400

    3 exercises

--- homework15.py
+++ homework15.py
@@ -32,3 +32,54 @@ for key in free_seats_key:
         free_seats[key] = value
         break
 print(free_seats)
+
+
+# 2. Write a Python program to find the highest 3 values in a dictionary
+
+dict1 = {"first":5, "second":89,"third":4, "forth":66, "fifth":2, "sixth": 7}
+
+
+dict1_list = []
+
+for value in dict1.values():
+    dict1_list.append(value)
+
+dict1_list.sort()
+
+print(dict1_list[-3:])
+
+
+
+
+# 3. Write a Python program to create a dictionary from a string.
+
+
+str1 = "a5d4l8f3y6"
+print(str1)
+
+dict1 = {}
+
+for i in range(0, len(str1), 2):
+     dict1[str1[i]] = str1[i+1]        
+
+print(dict1)
+
+
+
+# 3 version 2. Write a Python program to create a dictionary from a string.
+
+str1 = "asdasdasdasd"
+dict1 = {}
+print(str1)
+
+for i in str1:
+    if i in dict1.keys():
+        dict1[i] += 1
+    else:
+        dict1[i] = 1
+
+print(dict1)
+
+
+
+
