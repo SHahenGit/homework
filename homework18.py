195c490b59de4dac192669e737fe23c1b70cc7b3
SHahenGit <mehrabyanshahen@gmail.com>
2019-10-23 19:06:46 +0400

    exercise 1

---
+++ homework18.py
@@ -0,0 +1,26 @@
+# number 1
+
+class stringish:
+	def __init__(self, str1 = ""):
+		self.str1 = str1
+
+	def get_String(self):
+		self.str1 = input("enter a string: ")
+
+
+	def print_String(self):
+		
+		print(self.str1.upper())
+
+
+my_string = stringish()
+my_string.get_String()
+my_string.print_String()
+
+
+
+
+#number 2
+
+
+
